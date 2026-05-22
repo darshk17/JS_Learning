e7381e1180ce48835bedaf17cee5a7c08778a213
darshk17 <darshkhandelwal365@gmail.com>
2026-05-22 18:58:16 +0000

    Conversion and typecasting

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 12234
+// let is good
+let accountMail = "xako8804@gmail.com"
+// var is like global one,  can be changed anytime and dont use it
+var accountPassword = " 12345"
+accountCity = "Jaipur"
+console.log(accountCity);
+accountCity = "nagar"
+console.log(accountCity);
+console.log(3+3);
+let sahil= String(accountID);
+console.log(sahil);
+let Darshu= Number(accountMail);
+console.log(Darshu);
+// NAN  ->"33abc" conversion = NAN

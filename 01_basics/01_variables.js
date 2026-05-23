4284268fbb8da0ade5a749a8466968df4fa628fe
darshk17 <darshkhandelwal365@gmail.com>
2026-05-23 20:20:56 +0000

    Objects and Functions

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -13,3 +13,29 @@ console.log(sahil);
 let Darshu= Number(accountMail);
 console.log(Darshu);
 // NAN  ->"33abc" conversion = NAN
+console.log("3"+4/4);
+const dd= new Date();
+console.log(dd.getDay());
+let sum=0;
+for(i=0;i<=5;i++){
+    sum+=i;
+}
+console.log(sum);
+
+let teaCollection = [];
+let tea;
+
+do {
+    tea = prompt('Enter your favourite tea (type "stop" to finish)');
+
+    if (tea !== "stop") {
+        teaCollection.push(tea);
+    }
+
+} while (tea !== "stop");
+
+console.log(teaCollection);
+
+
+
+
---
+++ 01_basics/02.js
@@ -0,0 +1,18 @@
+// function Shanu(a,b) {
+//     return a*b;
+// }
+// let Shukla=Shanu(9,9);
+// console.log(Shukla);
+// console.log(Shanu);
+
+
+
+
+// // ->   ... is an rest operator and return ans in array format. It takes as many parameters user can give at the time of object initialize
+// function CalculatorAddCart(...price){
+//     return price
+// }
+//  console.log(CalculatorAddCart(100,1203,3677,76667,676767,6464,45654,6));
+
+
+
